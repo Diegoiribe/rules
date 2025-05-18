e488fa62542d3caef442043c032474456076b198
Diego Iribe <diegoiribe@192.168.68.105>
2025-05-18 00:40:05 -0700

    add confetti

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Ruleta from './components/Ruleta'
 function App() {
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100">
-      <h1 className="my-10 text-4xl font-bold">🎯 Ruleta de Premios</h1>
       <Ruleta />
     </div>
   )
--- src/components/Ruleta.tsx
+++ src/components/Ruleta.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Wheel } from 'react-custom-roulette'
+import Confetti from 'react-confetti'
 
 const baseOptions = [
   {
@@ -64,9 +65,11 @@ const Ruleta: React.FC = () => {
   const [mustSpin, setMustSpin] = useState(false)
   const [prizeNumber, setPrizeNumber] = useState(0)
   const [message, setMessage] = useState<string | null>(null)
+  const [showConfetti, setShowConfetti] = useState(false)
 
   const handleSpinClick = () => {
     setMessage(null) // Limpia el mensaje anterior
+    setShowConfetti(false) // Limpia el confetti anterior
     const newPrizeNumber = Math.floor(Math.random() * data.length)
     setPrizeNumber(newPrizeNumber)
     setMustSpin(true)
@@ -75,42 +78,62 @@ const Ruleta: React.FC = () => {
   const handleStopSpinning = () => {
     setMustSpin(false)
     setMessage(`¡Felicidades! Ganaste: ${data[prizeNumber].option}`)
+    setShowConfetti(true)
+
+    // Detiene el confetti después de unos segundos
+    setTimeout(() => setShowConfetti(false), 5000)
   }
 
   return (
-    <div className="flex flex-col items-center">
-      <Wheel
-        mustStartSpinning={mustSpin}
-        prizeNumber={prizeNumber}
-        data={data}
-        onStopSpinning={handleStopSpinning}
-        backgroundColors={[
-          '#FF0000',
-          '#FFA500',
-          '#FFFF00',
-          '#00FF00',
-          '#0000FF',
-          '#4B0082',
-          '#EE82EE'
-        ]}
-        textColors={['#ffffff']}
-        outerBorderColor="#000000"
-        outerBorderWidth={2}
-        innerRadius={0}
-        radiusLineColor="#ffffff"
-        radiusLineWidth={1}
-      />
-
-      <button
-        onClick={handleSpinClick}
-        className="px-6 py-2 mt-10 text-white bg-green-600 rounded-lg shadow-lg hover:bg-green-700"
-      >
-        ¡Girar Ruleta!
-      </button>
-
-      {message && (
-        <div className="mt-6 text-2xl font-bold text-green-700">{message}</div>
+    <div className="relative flex flex-col w-full">
+      {showConfetti && (
+        <Confetti width={window.innerWidth} height={window.innerHeight} />
       )}
+      <div className="flex flex-col items-center justify-center w-full">
+        <div className="max-w-2xl ">
+          <h1 className="my-10 text-4xl font-bold text-center">
+            🎯 Ruleta de Premios
+          </h1>
+
+          <Wheel
+            mustStartSpinning={mustSpin}
+            prizeNumber={prizeNumber}
+            data={data}
+            onStopSpinning={handleStopSpinning}
+            backgroundColors={[
+              '#FF0000',
+              '#FFA500',
+              '#FFFF00',
+              '#00FF00',
+              '#0000FF',
+              '#4B0082',
+              '#EE82EE'
+            ]}
+            textColors={['#ffffff']}
+            outerBorderColor="#000000"
+            outerBorderWidth={2}
+            innerRadius={2}
+            radiusLineColor="#ffffff"
+            radiusLineWidth={1}
+          />
+          <div className="flex justify-end w-full">
+            <button
+              onClick={handleSpinClick}
+              className="px-6 py-2 mt-10 text-white bg-green-600 rounded-lg shadow-lg hover:bg-green-700"
+            >
+              ¡Girar Ruleta!
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center w-full mt-5">
+        {message && (
+          <div className="mt-6 text-2xl font-bold text-green-700">
+            {message}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
